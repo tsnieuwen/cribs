84586c9e8d849c4829046b3f6f6362354b57bd28
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2020-12-09 13:56:38 -0700

    Draft iteration 3 complete

--- lib/house.rb
+++ lib/house.rb
@@ -13,4 +13,33 @@ attr_reader :price,
     @rooms << room_to_be_added
   end
 
+  def above_market_average?
+    true if @price > 500000
+  end
+
+  def rooms_from_category(room_category)
+    rooms_in_category = []
+    @rooms.each do |room|
+      if room.category == room_category
+        rooms_in_category << room
+      end
+    end
+      rooms_in_category
+  end
+
+  def area
+    total_area = 0
+    @rooms.each do |room|
+      total_area += room.area
+    end
+    total_area
+  end
+
+  def details
+    details_hash = Hash.new
+    details_hash["price"] = @price
+    details_hash["address"] = @address
+    details_hash
+  end
+
 end
